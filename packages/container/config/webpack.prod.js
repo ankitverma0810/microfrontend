5707c88117a106f0590b9373721257e879b502b4
Ankit Verma <ankitverma_0810@yahoo.co.in>
2021-07-29 19:50:03 +0530

    Added invalidation step

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -11,6 +11,8 @@ const prodConfig = {
 	output: {
 		filename: "[name].[contenthash].js",
 		/* Used by HtmlWebpackPlugin to prepend it in the script name before injecting it into the html document */
+		/* path should be same where we are placing our container files in the S3 bucket */
+		/* For reference refer container.yml file. aws s3 sync dist s3://${{ secrets.AWS_S3_BUCKET_NAME }}/container/latest */
 		publicPath: "/container/latest/",
 	},
 	plugins: [
