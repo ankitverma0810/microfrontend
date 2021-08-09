be8c493f72ce32bf130d35a812d31c6bca6f4220
Ankit Verma <ankitverma_0810@yahoo.co.in>
2021-08-09 14:48:31 +0530

    Fixed linear progress issue

--- packages/container/src/components/Progress.js
+++ packages/container/src/components/Progress.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { makeStyles, createStyles } from "@material-ui/core/styles";
-import { LinearProgress } from "@material-ui/core/LinearProgress";
+import { LinearProgress } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => {
 	return createStyles({
